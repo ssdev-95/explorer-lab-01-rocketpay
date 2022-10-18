1b26f1b4309980859e35df91d2777c3a0d295707
Salomao S <souza95salomao@gmail.com>
2022-10-18 21:58:51 +0000

    add: composable estates

---
+++ src/composables/brands.ts
@@ -0,0 +1,38 @@
+import { ref, Ref, watch } from 'vue'
+
+import Visa from '../assets/visa.svg'
+import Master from '../assets/master.svg'
+import Elo from '../assets/elo.svg'
+
+const brandIcons = {
+	master: Master,
+	visa: Visa,
+	elo: Elo,
+	default: null
+}
+
+const brand:Ref<'default'|'visa'|'master'|'elo'> = ref('elo')
+
+const colors = {
+	default: ['#202024', '#121214'],
+	visa: ['#436D99', '#2D57F2'],
+	master: ['#C69347', '#DF6F29'],
+	elo: ['#FFCB05', '#EF4123']
+}
+
+let ownerName:Ref<string> = ref('')
+let cardNumber:Ref<string> = ref('')
+let cvv:Ref<string> = ref('')
+let expiration:Ref<string> = ref('')
+
+watch(cardNumber, () => console.log(cardNumber))
+
+export {
+	ownerName,
+	cardNumber,
+	cvv,
+	expiration,
+	colors,
+	brand,
+	brandIcons
+}
