9bf36dccdeb932293f6a3885a6241122474fa153
Salomao S <souza95salomao@gmail.com>
2022-10-18 21:57:59 +0000

    add: new components

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,21 +1,30 @@
+<script setup lang="ts">
+  import Mask from './mask.vue'
+	import { ownerName, cardNumber, expiration, cvv, brandIcons, brand } from '../composables/brands'
+</script>
 <template>
-  <div class="relative flex-none border border-zinc-200 h-fit w-[375px] max-w-[95vw] flex flex-col gap-6 bg-glass p-5 rounded-xl text-zinc-100 overflow-hidden mb:mx-auto after:content[''] after:w-[80%] after:h-[50%] after:bg-[radial-gradient(#0000ff,transparent_70%)] after:absolute after:-top-[25%] after:left-[50%] after:-translate-x-[50%] after:z-10">
+  <div class="relative flex-none border border-zinc-200 h-fit w-[375px] max-w-[95vw] flex flex-col gap-6 bg-glass p-5 rounded-xl text-zinc-100 overflow-hidden mb:mx-auto">
+	  <Mask />
 	  <header class="z-20 flex items-center justify-between">
 		  <img src="../assets/subtract.svg" alt="" />
 
-			<img src="../assets/visa.svg" alt="" />
+			<img
+			  v-if="brand !== 'default'"
+				:src="brandIcons[brand]"
+				alt=""
+			/>
 		</header>
 
 		<div class="z-20 flex flex-col gap-6">
 		  <strong class="text-xl">
-			  1234 5678 9012 3456
+			  {{cardNumber ? cardNumber : '----------------------------'}}
 			</strong>
 
 			<p class="text-sm leading-normal">
 			  Onwer
 
 				<strong class="block">
-				  Salompas Senpai
+				  {{ownerName ? ownerName : '-------------------'}}
 				</strong>
 			</p>
 
@@ -24,7 +33,7 @@
 				  Expiration
 
 					<strong class="block ">
-					  08/30
+					  {{expiration ? expiration : '--/--'}}
 					</strong>
 				</p>
 
@@ -32,7 +41,7 @@
 				  CVV
 
 					<strong class="block">
-					  171
+					  {{cvv ? cvv : '------'}}
 					</strong>
 				</p>
 
--- src/components/form.vue
+++ src/components/form.vue
@@ -1,12 +1,17 @@
+<script setup lang="ts">
+  import { ownerName, cardNumber, expiration, cvv } from '../composables/brands'
+</script>
+
 <template>
   <form class="flex-1 max-w-[500px] text-zinc-100 flex flex-col gap-5">
 	  <label class="flex flex-col gap-2">
 		  <p class="text-sm">CARD NUMBER</p>
 
 			<input
-			  class="bg-zinc-900 w-full rounded-sm ring-1 ring-zinc-500 focus:ring-zinc-100 px-3 py-2"
+			  class="bg-zinc-900 w-full rounded ring-1 ring-zinc-500 focus:ring-zinc-100 px-3 py-2"
+				v-model="cardNumber"
+				placeholder="xxxx xxxx xxxx"
 			  name="cardNumber"
-				type="number"
 			/>
 		</label>
 
@@ -14,9 +19,10 @@
 		  <p class="text-sm">OWNER NAME</p>
 
 			<input
-			  class="bg-zinc-900 w-full border-0 rounded-sm ring-1 ring-zinc-500 focus:ring-zinc-100 px-3 py-2"
+			  class="bg-zinc-900 w-full border-0 rounded ring-1 ring-zinc-500 focus:ring-zinc-100 px-3 py-2"
+				v-model="ownerName"
+				placeholder="Salompas Senpai"
 			  name="ownerName"
-				type="text"
 			/>
 		</label>
 
@@ -25,9 +31,10 @@
 	  	  <p class="text-sm">EXPIRATION</p>
 
 		  	<input
-				  class="bg-zinc-900 w-full rounded-sm ring-1 ring-zinc-500 focus:ring-zinc-100 px-3 py-2"
+				  class="bg-zinc-900 w-full rounded ring-1 ring-zinc-500 focus:ring-zinc-100 px-3 py-2"
+					v-model="expiration"
+					placeholder="12/2077"
 					name="expiration"
-				  type="text"
   			/>
   		</label>
 
@@ -35,14 +42,15 @@
   		  <p class="text-sm">CVV</p>
 
   			<input
-  			  class="bg-zinc-900 w-full rounded-sm ring-1 ring-zinc-500 focus:ring-zinc-100 px-3 py-2"
+  			  class="bg-zinc-900 w-full rounded ring-1 ring-zinc-500 focus:ring-zinc-100 px-3 py-2"
+					v-model="cvv"
+					placeholder="666"
 	  			name="cvv"
-		  		type="number"
 			  />
   		</label>
 		</div>
 
-		<button class="bg-cyan-500 p-3 rounded-md text-zinc-900 font-semibold">
+		<button class="bg-cyan-500 p-3 rounded text-zinc-900 font-semibold">
 		  REGISTER CARD
 		</button>
 	</form>
